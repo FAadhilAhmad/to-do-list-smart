ce836ec99526634ecb192d0b449d5fb83a3ca780
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-05-10 21:41:47 +0700

    Update main.js
    masih error aduhh pening

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ let currentFilter = "all";
 
 function renderTasks() {
     tasksList.innerHTML ="";
-    let filterredTasks = taskInput.filter(task => {
+    let filteredTasks = taskInput.filter(task => {
         if (currentFilter === "done") return task.done;
         if (currentFilter === "todo") return !task.done;
         return true;
-    })
+    });
 
     filteredTasks.forEach((task, index  =>{
     const li = document.createElement("li");
@@ -29,3 +29,40 @@ function renderTasks() {
 localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+function addTask(){
+    const text = taskInput.value.trim();
+    if (text) {
+        tasks.push({ text, done: false});
+        tasksInput.value = "";
+        renderTasks();
+    }
+}
+
+function toggleDone(index) {
+    tasks[index].done =!tasks[index].done;
+    renderTasks();
+}
+
+function deleteTask(index){
+    tasks.splice(index, 1);
+    renderTasks();
+}
+
+addTaskBtn.addEventListener("click", addTask);
+taskInput.addEventListener("keypress", e => {
+    if(e.key === "enter") addTask()
+});
+filters.forEach(btn => {
+    btn.addEventListener("click",() => {
+        filters.forEach(f => f.remove("active"));
+        btn.classList.add("active");
+        currentFilter = btn.dataset.filter;
+        renderTasks();
+    });
+});
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+renderTasks();
